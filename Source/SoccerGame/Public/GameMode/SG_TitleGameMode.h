2a97d33a554a7b5f14652f9e81bc015f982746c9
bowonShin <bowo9777@naver.com>
2026-06-28 21:41:34 +0900

    feat: Rider
    라이더 플로그인 설치

---
+++ Source/SoccerGame/Public/GameMode/SG_TitleGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SG_TitleGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SOCCERGAME_API ASG_TitleGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
