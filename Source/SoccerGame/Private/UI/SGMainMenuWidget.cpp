7c7623fbfba082f1aac5f8aac54996530c1e50de
any0509 <annadevelop01@gmail.com>
2026-07-13 15:30:09 +0900

    🎉 add: SGmainMenuWidget 메인메뉴 위젯 담당 c++클래스 생성
    -✨ feat: GameStart 버튼 클릭 시 CreateServer() 함수 호출

---
+++ Source/SoccerGame/Private/UI/SGMainMenuWidget.cpp
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UI/SGMainMenuWidget.h"
+#include "Components/Button.h"
+#include "Multiplay/SGMultiplayGameInstance.h"
+#include "Kismet/GameplayStatics.h"
+
+
+void USGMainMenuWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+	
+
+	if (GameStartButton)
+	{
+		// 델리게이트 바인딩
+		GameStartButton->OnClicked.AddDynamic(this, &USGMainMenuWidget::OnGameStartButtonClicked);
+	}
+}
+
+void USGMainMenuWidget::OnGameStartButtonClicked()
+{
+	UGameInstance* GameInstance = GetGameInstance();
+	
+	USGMultiplayGameInstance* SGGameInstance = Cast<USGMultiplayGameInstance>(GameInstance);
+	
+	if (SGGameInstance)
+	{
+		SGGameInstance->CreateServer();
+	}
+}
