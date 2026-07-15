6b916a2cd6babc5fb318e67a1ae67958919d75a8
geoni-ara <lgeeckh@gmail.com>
2026-07-15 21:15:13 +0900

    Feat: item 획득 시 Effect 추가 + NS 위치 조정

--- Source/SoccerGame/SoccerGame.Build.cs
+++ Source/SoccerGame/SoccerGame.Build.cs
@@ -12,7 +12,7 @@ public class SoccerGame : ModuleRules
 		{
 			"Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput",
 			"GameplayAbilities", "GameplayTags", "GameplayTasks",
-			"UMG", "Slate", "SlateCore"
+			"UMG", "Slate", "SlateCore", "Niagara"
 		});
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
