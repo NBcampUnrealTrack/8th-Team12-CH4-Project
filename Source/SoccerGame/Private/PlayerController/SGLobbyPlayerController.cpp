778e3999aaf63ae94d7c0bd424b755bc678ecb63
any0509 <annadevelop01@gmail.com>
2026-07-20 12:08:51 +0900

    🐛 fix: ForceNetUpdate() 추가

--- Source/SoccerGame/Private/PlayerController/SGLobbyPlayerController.cpp
+++ Source/SoccerGame/Private/PlayerController/SGLobbyPlayerController.cpp
@@ -61,20 +61,6 @@ void ASGLobbyPlayerController::OnRep_PlayerState()
 	Super::OnRep_PlayerState();
 	
 	ASGLobbyPlayerState* LobbyPS = GetPlayerState<ASGLobbyPlayerState>();
-	const FString Message = FString::Printf(
-		TEXT("[LobbyPC] OnRep_PlayerState Local=%d PlayerState=%s Class=%s CastLobbyPS=%d"),
-		IsLocalController(),
-		*GetNameSafe(PlayerState),
-		PlayerState ? *PlayerState->GetClass()->GetName() : TEXT("None"),
-		IsValid(LobbyPS)
-	);
-	
-	UE_LOG(LogTemp, Warning, TEXT("%s"), *Message);
-	
-	if (GEngine)
-	{
-		GEngine->AddOnScreenDebugMessage(-1, 10.f, IsValid(LobbyPS) ? FColor::Green : FColor::Red, Message);
-	}
 	
 	if (IsLocalController() && IsValid(LobbyPS))
 	{
